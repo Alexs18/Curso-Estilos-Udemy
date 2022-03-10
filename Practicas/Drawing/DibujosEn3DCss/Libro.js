a0cf8cda5fcdcc37cebe8ceb998a7052289b4800
alex lopez <mclexis18@gmail.com>
2022-03-10 15:36:40 -0500

    Libro 3D con css

---
+++ Practicas/Drawing/DibujosEn3DCss/Libro.js
@@ -0,0 +1,11 @@
+var lista = document.getElementById("VerMenu").addEventListener("click", ()=>{
+
+    document.getElementById("PrimerHoja").classList.toggle("PortadaEfecto");
+
+});
+var lista = document.getElementById("Menu2").addEventListener("click", ()=>{
+
+    document.getElementById("Portada").classList.toggle("PortadaEfecto");
+    document.getElementById("Portada").style = "z-index:1";
+
+});

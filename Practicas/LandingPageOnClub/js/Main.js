0350a5dc76c4c6a8ef9c3d59110a0f79634da37e
alex lopez <mclexis18@gmail.com>
2022-03-15 23:48:01 -0500

    Seccion Menu Cocteles 90% Falta Menu Bebidas Clasicas

--- Practicas/LandingPageOnClub/js/Main.js
+++ Practicas/LandingPageOnClub/js/Main.js
@@ -2,20 +2,25 @@
 function LibroRotador(identificador) {
     identificador.classList.toggle("EfectoHoja");
 }
-function OcultarPanorama(identificador) {
-    identificador.style = "z-index:-1";
+function OcultarPanorama(identificador, zindex) {
+    identificador.style = `z-index:${zindex}`;
 }
 
 let PuertasArray = document.querySelectorAll("#AbrirLibro");
 let PortadasArray = document.querySelectorAll("#Catalogo__Cocteles__Contenido");
 
+console.log(PortadasArray.length);
+console.log(PuertasArray.length);
 
-for (let index = 0; index < PuertasArray.length; index++) {
+
+for (let index0 = 0; index0 < PuertasArray.length; index0++) {
     
-    PuertasArray[index].addEventListener("click", ()=>{
-        LibroRotador(PortadasArray[index]);
-        OcultarPanorama(PortadasArray[index+1]);
-        console.log(index);
+    console.log(index0);
+    PuertasArray[index0].addEventListener("click", ()=>{
+        console.log(index0);
+        LibroRotador(PortadasArray[index0]);
+        console.log(PortadasArray[index0]);
+        OcultarPanorama(PortadasArray[index0+1], -(index0+1));
     }); 
 
 }
